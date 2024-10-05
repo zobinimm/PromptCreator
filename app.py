9107929e2d809d2a0bd18453d04d66859dae4861
zobinimm <z0bin@163.com>
2024-10-05 20:11:39 +0800

    first version

--- app.py
+++ app.py
@@ -382,52 +382,54 @@ def create_film_audio():
 
 @app.route('/createfilmdraft', methods=['POST'])
 def create_film_draft():
-    tutorial_asset_dir = os.path.join(os.path.dirname(__file__), 'libs', 'pyJianYingDraft', 'readme_assets', 'tutorial')
+    data = request.get_json()
+    pro_name = data.get('pro_name')
+    draft_path = data.get('draft_path')
+    data_path = data.get('data_path')
+    width = data.get('width')
+    height = data.get('height')
 
+    dump_path  = os.path.join(os.path.dirname(data_path), 'Draft')
+    if not os.path.exists(dump_path):
+        os.makedirs(dump_path)
+
+
+    with open(data_path, 'r', encoding='utf-8') as file:
+        json_data = json.load(file)
+
+    start_audio_time = 0
     # 创建剪映草稿
-    script = draft.Script_file(1080, 1080)  # 1080x1080分辨率
-
+    script = draft.Script_file(width, height)
+    script.add_meta_info(os.path.join(draft_path, pro_name))
     # 添加音频、视频和文本轨道
     script.add_track(draft.Track_type.audio).add_track(draft.Track_type.video).add_track(draft.Track_type.text)
+    # 遍历每个元素
+    for item in json_data:
+        original_text = item.get('OriginalText')
+        audio_path = item.get('AudioPath')
+        audio_time = item.get('AudioTime')
 
-    # 从本地读取音视频素材和一个gif表情包
-    audio_material = draft.Audio_material(os.path.join(tutorial_asset_dir, 'audio.mp3'))
-    video_material = draft.Video_material(os.path.join(tutorial_asset_dir, 'video.mp4'))
-    sticker_material = draft.Video_material(os.path.join(tutorial_asset_dir, 'sticker.gif'))
-    script.add_material(audio_material).add_material(video_material).add_material(sticker_material)  # 随手添加素材是好习惯
+        image_path = ''
+        for img_group in item.get('ImageGroup', []):
+            if img_group.get('IsCheckedImg'):
+                image_path = img_group.get('ImagePath')
+                break
 
-    # 创建音频片段
-    audio_segment = draft.Audio_segment(audio_material,
-                                        trange("0s", "5s"),  # 片段将位于轨道上的0s-5s（注意5s表示持续时长而非结束时间）
-                                        volume=0.6)  # 音量设置为60%(-4.4dB)
-    audio_segment.add_fade("1s", "0s")  # 增加一个1s的淡入
+        audio_material = draft.Audio_material(audio_path)
+        sticker_material = draft.Video_material(image_path)
+        script.add_material(audio_material).add_material(sticker_material)
+        audio_segment = draft.Audio_segment(audio_material, trange(start_audio_time, audio_material.duration))
+        sticker_segment = draft.Video_segment(sticker_material, trange(start_audio_time, audio_material.duration))
+        script.add_segment(audio_segment).add_segment(sticker_segment)
+        text_segment = draft.Text_segment(original_text, trange(start_audio_time, audio_material.duration),
+                                          style=draft.Text_style(color=(1.0, 1.0, 0.0)),
+                                          clip_settings=draft.Clip_settings(transform_y=-0.8))
+        script.add_segment(text_segment)
+        start_audio_time += audio_material.duration
 
-    # 创建视频片段
-    video_segment = draft.Video_segment(video_material,
-                                        trange("0s", "4.2s"))  # 片段将位于轨道上的0s-4.2s（取素材前4.2s内容，注意此处4.2s表示持续时长）
-    video_segment.add_animation(Intro_type.斜切)  # 添加一个入场动画“斜切”
-
-    sticker_segment = draft.Video_segment(sticker_material,
-                                          trange(video_segment.end, sticker_material.duration))  # 紧跟上一片段，长度与gif一致
-
-    # 为二者添加一个转场
-    video_segment.add_transition(Transition_type.信号故障)  # 注意转场添加在“前一个”视频片段上
-
-    # 将上述片段添加到轨道中
-    script.add_segment(audio_segment).add_segment(video_segment).add_segment(sticker_segment)
-
-    # 创建一行类似字幕的文本片段并添加到轨道中
-    text_segment = draft.Text_segment("据说pyJianYingDraft效果还不错?", trange(0, script.duration),
-                                      # 文本将持续整个视频（注意script.duration在上方片段添加到轨道后才会自动更新）
-                                      style=draft.Text_style(color=(1.0, 1.0, 0.0)),  # 设置字体颜色为黄色
-                                      clip_settings=draft.Clip_settings(transform_y=-0.8))  # 模拟字幕的位置
-    script.add_segment(text_segment)
-
-    # 保存草稿（覆盖掉原有的draft_content.json）
-    script.dump("draft_content.json")
-
+    script.dump(dump_path)
     response_data = {
-        "file_path": "draft_content.json"
+        "file_path": dump_path
     }
     return jsonify(response_data)
 
